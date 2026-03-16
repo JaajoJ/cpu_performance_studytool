5e20c4f7b4b01a8b2f3343e3962ee5d805ae2003
JaajoJ <71885933+JaajoJ@users.noreply.github.com>
2026-03-16 13:03:40 +0200

    Increase sleep duration from 1 to 5 seconds

--- studytool/st_idle_freq.c
+++ studytool/st_idle_freq.c
@@ -136,7 +136,7 @@ int st_idle_freq_get_core_idle_delta(PackageStats * package_stats)
         get_core_idle(c_states_time[core_number], core_number, package_stats->available_idle_states);
     }
 
-    sleep(1);
+    sleep(5);
 
     for (int core_number = 0; core_number < package_stats->all_cpus; ++core_number)
     {
