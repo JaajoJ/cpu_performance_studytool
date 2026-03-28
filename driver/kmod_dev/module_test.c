2f82955f5c80584be7392b278739e1d0a1483510
jj <email@email.com>
2026-03-28 13:52:37 +0200

    removed libraries

--- driver/kmod_dev/module_test.c
+++ driver/kmod_dev/module_test.c
@@ -5,7 +5,7 @@
 #include <linux/uaccess.h>
 #include <linux/cpu.h>
 #include <linux/cpuidle.h>
-#include "st_lib.h"
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("You");
 MODULE_DESCRIPTION("Reads a 32-bit integer from userspace and prints it");
@@ -36,6 +36,38 @@ static struct device_attribute c_state_attr = {     // Device file for /sys/clas
     .store = value_store,
 };
 
+static void set_core_name(char * buf, int value)
+{
+    buf[0] = 'c';
+    buf[1] = 'o';
+    buf[2] = 'r';
+    buf[3] = 'e';
+    int offset = 4;
+    if(value > 99)
+    {
+        buf[offset] = value / 100 + '0';
+        value = value % 100;
+        ++offset;
+    }
+    if(value > 9)
+    {
+        buf[offset] = value / 10 + '0';
+        value = value % 10;
+        ++offset;
+    }
+
+    buf[offset] = value + '0';
+    ++offset;
+
+    buf[offset] = '\0';
+
+}
+
+struct stDev {
+    struct device * dev;
+    char core_name[8];
+    int state;
+};
 
 static int __init hello_init(void)
 {
